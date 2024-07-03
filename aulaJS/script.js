40e5e907696fcd02a8b6110059754c5f27a2eb84
Edenilson Fabiano <20231ITA01GB0011@alunos.ifbaiano.edu.br>
2024-07-03 00:41:13 -0300

    Em  desenvolvimento

--- aulaJS/script.js
+++ aulaJS/script.js
@@ -1,15 +1,36 @@
 window.onload = function() {
-    document.getElementById('azul').addEventListener("mouseover", function() {
-        document.getElementById('titulo').style.color = 'blue';
+    document.getElementById('val2').addEventListener('focusout', function() {
+        val1 = document.getElementById("val1").value;
+        val2 = document.getElementById("val2").value;
+    
+        document.getElementById('result1').value = parseInt(val1) + parseInt(val2);
     })
 
-    document.getElementById('vermelho').addEventListener("mouseout", function() {
-        document.getElementById('titulo').style.color = 'red';
+    document.getElementById('val4').addEventListener("focusout", function() {
+        val3 = document.getElementById("val3").value;
+        val4 = document.getElementById("val4").value;
+
+        document.getElementById('result2').value = parseInt(val3) - parseInt(val4);
     })
-    document.getElementById('verde').addEventListener("mouseup", function() {
-        document.getElementById('titulo').style.color = 'green';
+
+    document.getElementById('val6').addEventListener("focusout", function() {
+        val5 = document.getElementById("val5").value;
+        val6 = document.getElementById("val6").value;
+
+        document.getElementById('result3').value = parseInt(val5) * parseInt(val6);
     })
-    document.getElementById('amarelo').addEventListener('click', function() {
-        document.getElementById('titulo').style.color = 'yellow';
+
+    document.getElementById('val8').addEventListener("focusout", function() {
+        val7 = document.getElementById("val7").value;
+        val8 = document.getElementById("val8").value;
+
+        document.getElementById('result4').value = parseInt(val7) / parseInt(val8);
+    })
+
+
+
+    elemento = document.getElementById('val1');
+    elemento.addEventListener("click", function() {
+        elemento.value = "";
     })
 }
